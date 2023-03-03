d19d30c50959c8d8f9354c407a65c75a3cd5be3a
Raul França <raulmfranca@gmail.com>
2023-03-03 18:44:12 -0300

    test more queries on main.go

--- cmd/sqlc/main.go
+++ cmd/sqlc/main.go
@@ -24,7 +24,7 @@ func main() {
 	defer dbConn.Close()
 
 	queries := db.New(dbConn)
-	reslt, err := queries.CreateTodo(ctx, db.CreateTodoParams{
+	_, err = queries.CreateTodo(ctx, db.CreateTodoParams{
 		Name:        sql.NullString{String: "Tirar o lixo", Valid: true},
 		Description: sql.NullString{String: "Recolher os lixos de casa e levar para fora.", Valid: true},
 		Done:        sql.NullBool{Bool: false},
@@ -33,9 +33,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error to create new Todo: %v", err)
 	}
+	//
+	//fmt.Printf("Nova tarefa add, id:%d, %v \n", reslt.ID, reslt.Name.String)
+	//fmt.Printf("Descrição: %v \n", reslt.Description.String)
+	//fmt.Printf("Criada em : %v, Status: %v\n", reslt.CreatedAt.Format("02/01/06 as 15:04"), reslt.Done.Bool)
 
-	fmt.Printf("Nova tarefa add, id:%d, %v \n", reslt.ID, reslt.Name.String)
-	fmt.Printf("Descrição: %v \n", reslt.Description.String)
-	fmt.Printf("Criada em : %v, Status: %v\n", reslt.CreatedAt.Format("02/01/06 as 15:04"), reslt.Done.Bool)
+	fmt.Println("-------GetAllTodo------")
+	listTodos, err := queries.GetAllTodo(ctx)
+	for _, todo := range listTodos {
+
+		fmt.Printf("Tarefa id:%d, %v \n", todo.ID, todo.Name.String)
+		fmt.Printf("Descrição: %v \n", todo.Description.String)
+		fmt.Printf("Criada em : %v, Status: %v\n", todo.CreatedAt.Format("02/01/06 as 15:04"), todo.Done.Bool)
+	}
+	fmt.Println("-------GetTodo------")
+	todo, err := queries.GetTodo(ctx, listTodos[0].ID)
+	if err != nil {
+		log.Fatalf("Error: ToDo Not Found: %w", err)
+	}
+	fmt.Printf("test GetTodo: %s", todo.Name.String)
 
 }
